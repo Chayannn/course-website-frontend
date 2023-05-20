458ed371950d2e1b20b6df6bf7bccf72494d6987
Chayannn <chayanp188@gmail.com>
2023-05-20 21:28:13 +0530

    Created initial landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from 'react';
+import {
+  Heading,
+  Stack,
+  VStack,
+  Text,
+  Button,
+  Image,
+  Box,
+  HStack,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+import vg from '.././assets/images/bg.png';
+import introVideo from '../assets/videos/intro.mp4';
 
-function Home() {
+import { CgGoogle, CgFacebook } from 'react-icons/cg';
+import { SiCoursera, SiUdemy } from 'react-icons/si';
+import { DiAws } from 'react-icons/di';
+
+export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
-
-export default Home
+    <section className="home">
+      <div className="container">
+        <Stack
+          direction={['column', 'row']}
+          height="100%"
+          justifyContent={['center', 'space-between']}
+          alignItems="center"
+          spacing={['16', '18', '20', '56']}
+        >
+          <VStack width={'full'} alignItems={['center', 'flex-end']}>
+            <Heading children="LEARN FROM THE EXPERTS" size={'2xl'} />
+            <Text children="Find Valuable Content At Resonable Price" />
+            <Link to="/courses">
+              <Button size={'lg'} colorScheme="yellow">
+                Explore Now
+              </Button>
+            </Link>
+          </VStack>
+          <Image
+            className="vector-graphics"
+            boxSize={'md'}
+            src={vg}
+            objectFit="contain"
+          />
+        </Stack>
+      </div>
+      <Box padding={'8'} bg="blackAlpha.800">
+        <Heading
+          textAlign={'center'}
+          children="OUR BRANDS"
+          fontFamily="body"
+          color="yellow.400"
+        ></Heading>
+        <HStack
+          className="brandsBanner"
+          justifyContent={'space-evenly'}
+          marginTop="4"
+        >
+          <CgGoogle />
+          <CgFacebook />
+          <SiCoursera />
+          <SiUdemy />
+          <DiAws />
+        </HStack>
+      </Box>
+      <div className="video-container">
+        <video
+          autoPlay
+          controls
+          controlsList="nodownload nofullscreen noremoteplayback"
+          disablePictureInPicture
+          disableRemotePlayback
+          src={introVideo}
+        ></video>
+      </div>
+    </section>
+  );
+};
+export default Home;
