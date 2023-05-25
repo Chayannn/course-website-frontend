64c8cba83f476f716321f21d9c5defd2659abd8c
Chayannn <chayanp188@gmail.com>
2023-05-25 18:14:51 +0530

    Added Payments page and course detail page

--- src/components/Layout/NotFound/NotFound.jsx
+++ src/components/Layout/NotFound/NotFound.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {
+  Button,
+  Container,
+  Heading,
+  VStack,
+} from '@chakra-ui/react';
+import { RiErrorWarningFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <Container h={'90vh'}>
+      <VStack justifyContent={'center'} h={'full'} spacing={'4'}>
+        <RiErrorWarningFill size={'5rem'} />
+        <Heading>Page Not Found</Heading>
+        <Link to={'/'}>
+          <Button variant={'ghost'}>Go to Home</Button>
+        </Link>
+      </VStack>
+    </Container>
+  );
+};
 
-export default NotFound
+export default NotFound;
