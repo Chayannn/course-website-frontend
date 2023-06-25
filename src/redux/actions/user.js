eea4a98d67b56eef018af8d6d7b4b82d5b7b89d0
Chayannn <chayanp188@gmail.com>
2023-06-25 10:19:31 +0530

    added register functionality

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -20,6 +20,25 @@ export const login = (email, password) => async dispatch => {
     dispatch({ type: 'loginFail', payload: error.response.data.message });
   }
 };
+export const register = (formdata) => async dispatch => {
+  try {
+    dispatch({ type: 'registerRequest' });
+
+    const { data } = await axios.post(
+      `${server}/register`,
+      formdata,
+      {
+        headers: {
+          'Content-type': 'multipart/form-data',
+        },
+        withCredentials: true,
+      }
+    );
+    dispatch({ type: 'registerSuccess', payload: data });
+  } catch (error) {
+    dispatch({ type: 'registerFail', payload: error.response.data.message });
+  }
+};
 
 export const loadUser = () => async dispatch => {
   try {
